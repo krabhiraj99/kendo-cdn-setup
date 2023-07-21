7ef55ce0ec02ac0c59ec481182b697d55e8e9d5f
Abhiraj Kumar <abhiraj.kumar@vince.no>
2023-07-21 16:21:22 +0530

    Added grid

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,36 +1,91 @@
 
 <template>
   <div>
-    <kendo-grid :data-items="gridData" :columns="gridColumns">
-      <template v-slot:ageTemp="{ props }">
-        <td>
-          {{ props.dataItem }}
-        </td>
-      </template>
-    </kendo-grid>
+    <k-dropdownlist
+        :data-items='sports'
+        :default-item="'Select sport ...'"
+    ></k-dropdownlist>
+    <k-input :style="{ width: '230px' }" :label="'First name'"></k-input>
+      <k-grid
+                 :data-items="items"
+                 :skip= "skip"
+                 :take="take"
+                 :total="total"
+                 :pageable="pageable"
+                 :page-size="pageSize"
+                 v-on:pagechange="pageChangeHandler"
+                 :sortable="true"
+                  :sort="sort"
+                  @sortchange="sortChangeHandler"
+                 :columns = "columns">
+      </k-grid>
+      
   </div>
 </template>
 
 <script>
-export default {
-  name: "demo-table",
-  components: {
-    "kendo-grid": window.KendoVueGrid.Grid,
-  },
-  data() {
-    return {
-      gridData: [
-        { id: 1, name: "John Doe", age: 30 },
-        { id: 2, name: "Jane Smith", age: 25 },
-        { id: 3, name: "Bob Johnson", age: 40 },
+import { orderBy } from '@progress/kendo-data-query';
+  export default{
+    // components: {
+    //   'k-grid': window.KendoVueGrid.Grid,
+    //   'k-dropdownlist': window.KendoVueDropdowns.DropDownList,
+    //   'k-input':window.KendoVueInputs.Input,
+    // },
+    data(){
+      return {
+      skip: 0,
+      take: 10,
+      products: [],
+      sort: [],
+      pageSize: 10,
+      pageable: {
+        buttonCount: 5,
+        info: true,
+        type: 'numeric',
+        pageSizes: true,
+        previousNext: true
+      },
+      columns: [
+        { field: 'ProductID'},
+        { field: 'ProductName', title: 'Product Name' },
+        { field: 'UnitPrice', title: 'Unit Price' }
       ],
-      gridColumns: [
-        { field: "id",title: "ID" },
-        { field: "age", title: "Age", cell: "ageTemp" },
-        { field: "name", title: "Name" },
-      ],
+      sports: [ 'Basketball', 'Football', 'Tennis', 'Volleyball' ],
+      selectedFruit: null,
+      }
+    },
+    computed: {
+      items: {
+        get: function() {
+          return orderBy(this.products.slice(this.skip, this.take + this.skip),this.sort);
+        }
+      },
+      total () {
+        return this.products ? this.products.length : 0;
+      }
+    },
+    methods: {
       
-    };
-  },
-};
+      pageChangeHandler: function(event) {
+        this.skip = event.page.skip;
+        this.take = event.page.take;
+      },
+      sortChangeHandler(e){
+        this.sort = e.sort;
+      },
+      createRandomData(count) {
+        const productNames = ['Chai', 'Chang', 'Syrup', 'Apple', 'Orange', 'Banana', 'Lemon', 'Pineapple', 'Tea', 'Milk'];
+        const unitPrices = [12.5, 10.1, 5.3, 7, 22.53, 16.22, 20, 50, 100, 120]
+
+        return Array(count).fill({}).map((_, idx) => ({
+          ProductID: idx + 1,
+          ProductName: productNames[Math.floor(Math.random() * productNames.length)],
+          UnitPrice: unitPrices[Math.floor(Math.random() * unitPrices.length)]
+        }));
+      }
+    },
+    mounted() {
+      this.products = this.createRandomData(1000);
+    }
+  }
 </script>
--- src/main.js
+++ src/main.js
@@ -5,7 +5,11 @@ import App from "./App.vue";
 
 // Mount the app instance to the '#app' element
 Vue.config.productionTip = false;
+Vue.component('k-grid', window.KendoVueGrid.Grid);
+Vue.component('k-dropdownlist', window.KendoVueDropdowns.DropDownList);
+Vue.component('k-input',window.KendoVueInputs.Input);
 
+console.log("Vue.options", Vue.options);
 new Vue({
   render: (h) => h(App),
 }).$mount("#app");
